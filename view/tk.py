313dddad31c75811e3b27021c1a6bb09e47d1418
AresGG1 <taraswestgoter@gmail.com>
2022-12-11 20:33:14 +0200

    view additions

--- view/tk.py
+++ view/tk.py
@@ -1,5 +1,7 @@
 import tkinter as tk
 from tkinter import ttk
+from tkinter import messagebox
+
 
 
 # class Grid(ttk.Treeview):
@@ -10,7 +12,7 @@ class App(tk.Tk):
     super().__init__()
 
     self.title('My Awesome App')
-    self.geometry('1000x800')
+    self.geometry('1000x640')
 
 
 
@@ -28,7 +30,7 @@ for tab in tabs:
     for r in range(4): tab.rowconfigure(index=r, weight=30)
     count += 1
 columnsCustomer = ('customer_id', 'name', 'email', 'age', 'address')
-columnsItem = ('product_id', 'name', 'sku', 'sku', 'category')
+columnsItem = ('product_id', 'name', 'sku', 'price', 'category')
 columnsOrder = ('order_id', 'created_at', 'quantity', 'customer_id', 'item_id')
 gridCustomer = ttk.Treeview(tabCustomer, columns=columnsCustomer, show='headings')
 gridItem = ttk.Treeview(tabItem, columns=columnsItem, show='headings')
@@ -46,31 +48,49 @@ buttons_list = [[],[],[]]
 
 col = 0
 
+def click00():
+    text = entries[0][0].get()
+    messagebox.showinfo("Message","text")
+
+
+func = [click00]
+
+
 gridCustomer.pack()
 gridItem.pack()
 gridOrder.pack()
 for i in tabs:
     for btn in buttons:
-        button = ttk.Button(i, text=btn)
+        button = ttk.Button(i, text=btn, command=func[0])
         button.pack()
         buttons_list[col].append(button)
     col += 1
+
+count = 0
 entries = [[],[],[]]
 for i in columnsCustomer:
-    entry = tk.Entry(tabControl)
-    entry.place()
+    entry = tk.Entry(tabCustomer)
+    entry.place(relx=0.15 + 0.15*count, rely=0.7)
     entries[0].append(entry)
+    count += 1
 
+count = 0
 for i in columnsItem:
-    entry = tk.Entry(tabControl)
+    entry = tk.Entry(tabItem)
+    entry.place(relx=0.15 + 0.15*count, rely=0.7)
     entries[1].append(entry)
+    count += 1
 
+count = 0
 for i in columnsOrder:
-    entry = tk.Entry(tabControl)
+    entry = tk.Entry(tabOrder)
+    entry.place(relx=0.15 + 0.15*count, rely=0.7)
     entries[2].append(entry)
-
+    count += 1
 tabControl.pack(expand=1, fill="both")
 
+
+# buttons_list[0][0].
 
 
 main.mainloop()

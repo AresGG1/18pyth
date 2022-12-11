942c77b35d4aa305429ce74b1780613e0ca60b3a
AresGG1 <taraswestgoter@gmail.com>
2022-12-11 19:55:29 +0200

    view additions

--- view/tk.py
+++ view/tk.py
@@ -10,16 +10,16 @@ class App(tk.Tk):
     super().__init__()
 
     self.title('My Awesome App')
-    self.geometry('600x480')
+    self.geometry('1000x800')
 
 
 
 main = App()
 tabControl = ttk.Notebook(main)
-tab1 = ttk.Frame(tabControl)
-tab2 = ttk.Frame(tabControl)
-tab3 = ttk.Frame(tabControl)
-tabs = (tab1, tab2, tab3)
+tabCustomer = ttk.Frame(tabControl)
+tabItem = ttk.Frame(tabControl)
+tabOrder = ttk.Frame(tabControl)
+tabs = (tabCustomer, tabItem, tabOrder)
 
 count = 1
 for tab in tabs:
@@ -27,8 +27,48 @@ for tab in tabs:
     for c in range(4): tab.columnconfigure(index=c, weight=30)
     for r in range(4): tab.rowconfigure(index=r, weight=30)
     count += 1
-    btn = ttk.Button(tab1)
-    btn.pack()
+columnsCustomer = ('customer_id', 'name', 'email', 'age', 'address')
+columnsItem = ('product_id', 'name', 'sku', 'sku', 'category')
+columnsOrder = ('order_id', 'created_at', 'quantity', 'customer_id', 'item_id')
+gridCustomer = ttk.Treeview(tabCustomer, columns=columnsCustomer, show='headings')
+gridItem = ttk.Treeview(tabItem, columns=columnsItem, show='headings')
+gridOrder = ttk.Treeview(tabOrder, columns=columnsOrder, show='headings')
+for column in columnsCustomer:
+    gridCustomer.heading(column, text=column)
+for column in columnsItem:
+    gridItem.heading(column, text=column)
+for column in columnsOrder:
+    gridOrder.heading(column, text=column)
+
+buttons = ('add', 'update', 'delete', 'search')
+
+buttons_list = [[],[],[]]
+
+col = 0
+
+gridCustomer.pack()
+gridItem.pack()
+gridOrder.pack()
+for i in tabs:
+    for btn in buttons:
+        button = ttk.Button(i, text=btn)
+        button.pack()
+        buttons_list[col].append(button)
+    col += 1
+entries = [[],[],[]]
+for i in columnsCustomer:
+    entry = tk.Entry(tabControl)
+    entry.place()
+    entries[0].append(entry)
+
+for i in columnsItem:
+    entry = tk.Entry(tabControl)
+    entries[1].append(entry)
+
+for i in columnsOrder:
+    entry = tk.Entry(tabControl)
+    entries[2].append(entry)
+
 tabControl.pack(expand=1, fill="both")
 
 
